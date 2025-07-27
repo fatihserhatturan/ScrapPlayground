6e7145b4660d62ee88aea5c59aac06ed4ad4857f
fatih serhat turan <95492309+fatihserhatturan@users.noreply.github.com>
2025-07-27 13:51:38 +0300

    Update generateReport.js

--- src/scripts/generateReport.js
+++ src/scripts/generateReport.js
@@ -4,19 +4,16 @@ import { launch } from 'chrome-launcher';
 async function generateReport(url) {
   const chrome = await launch({ chromeFlags: ['--headless'] });
 
-  // Lighthouse'u çalıştır
   const options = {
     logLevel: 'info',
     output: 'json',
-    onlyCategories: ['performance'], // Performans kategorisine odaklanmak için
+    onlyCategories: ['performance'], 
     port: chrome.port,
   };
 
   const runnerResult = await lighthouse(url, options);
 
-  // Lighthouse raporunu JSON dosyasına yaz
-  const reportJson = JSON.stringify(runnerResult.lhr, null, 2);  // JSON formatına çeviriyoruz
-
+  const reportJson = JSON.stringify(runnerResult.lhr, null, 2); 
 
   await chrome.kill();
 
